2eb8152a8bd26d69ed0103988a79cbc765d418d9
annie <yupito22@gmail.com>
2025-02-23 22:07:01 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("hola mundo desde node");
+
+require('dotenv').config()
+const cors = require('cors') //cors para permitir peticiones desde otros dominios
+
+const sequelize = require('./models/database/dbconneciont') //conectar a la base de datos
+
+const express = require('express')
+const app = express() //crear una app de express
+
+const port = process.env.PORT || 3000
+
+app.use(cors()) //cors
+app.listen(port, () => {
+  console.log('My app en node en el puerto ' + port)
+
+  //conectarse a la base de datos
+  sequelize
+    .sync({ force: false })
+    .then(() => {
+      console.log('conectado a la db')
+    })
+    .catch((error) => {
+      console.log('Ha habido un error en la conexion: ', +error)
+    })
+})
